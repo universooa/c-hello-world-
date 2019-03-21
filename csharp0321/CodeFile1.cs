8c6c188b7f8ffb8f7caade51882470deeb2a6ade
universooa <41477328+universooa@users.noreply.github.com>
2019-03-21 11:14:32 +0900

    using system.console 추가

---
+++ csharp0321/CodeFile1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+
+namespace FirstCSharp
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                WriteLine("사용법:HelloWorld.exe <이름>");
+                return;
+            }
+            WriteLine("Hello,{0}!", args[0]);
+        }
+    }
+}
